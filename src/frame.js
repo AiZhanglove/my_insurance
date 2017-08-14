f9636b62d79fd2589afa6f4cff5ed04695291c3c
zhangai <zhangai@xiaomi.com>
2017-08-14 10:29:51 +0800

    buyy

--- src/frame.js
+++ src/frame.js
@@ -5,6 +5,7 @@ import {Router, Route, hashHistory, IndexRoute } from "react-router";
 import methods from './assets/methods.js';
 import FrameDetail from './frame/FrameDetail.jsx';
 import Inform from './frame/Inform.jsx';
+import FrameBuy from './frame/FrameBuy.jsx';
 
 //链接拦截
 methods.linkInterceptor();
@@ -23,6 +24,7 @@ var routes = (
 		<Route path="/" component={App}>
 			<Route path="detail/:id" component={FrameDetail}></Route>
 			<Route path="inform/:id" component={Inform} />
+			<Route path="buy/:id" component={FrameBuy} />
 		</Route>
 	</Router>
 )
